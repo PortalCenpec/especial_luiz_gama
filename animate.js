cefbcc7ceab2b09b9aeceb7072c887d5af7977b9
PortalCenpec <151966383+PortalCenpec@users.noreply.github.com>
2023-11-27 17:48:17 -0300

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,13 +1,10 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-});
-
-const hiddenSection = document.querySelectorAll('.hidden');
-hiddenSection.forEach((el) => observer.observe(el));
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
